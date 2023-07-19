5a975a75623eb640f9cc770c861d3624d6c74cdf
sungchanseo <seobanquet@gmail.com>
2023-07-19 12:55:10 +0900

    타임리프 연습

--- src/main/java/com/fastcampus/ch2/HomeController.java
+++ src/main/java/com/fastcampus/ch2/HomeController.java
@@ -1,8 +1,12 @@
 package com.fastcampus.ch2;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
 
 //1. 원격프로그램으로 먼저 등록 -> @Controller 어노테이션 추가
 //2. URL과 메소드를 연결 -> @RequestMapping
@@ -14,5 +18,14 @@ public class HomeController { //원격프로그램
     public String main(){
         System.out.println("hello");
         return "index"; // template/index.html 보통 메인페이지를 index.html로 한다.
+    }
+
+    @GetMapping("/test")
+    public String test(Model model){
+        model.addAttribute("lastName", "SEO");
+        model.addAttribute("firstName", "SUNG CHAN");
+        model.addAttribute("list", Arrays.asList("aaa", "bbb", "ccc", "ddd"));
+        model.addAttribute("bno", "1234");
+        return "test"; // template/test.html
     }
 }
--- src/main/java/com/fastcampus/ch2/LoginController.java
+++ src/main/java/com/fastcampus/ch2/LoginController.java
@@ -6,7 +6,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.servlet.http.HttpServletRequest;
 import java.net.URLEncoder;
 
 @Controller
@@ -23,15 +25,27 @@ public class LoginController {
 //    @RequestMapping(value="/login/login", method = RequestMethod.POST)
 //    @RequestMapping(value="/login/login", method = {ReqeustMethod.GET, RequestMethod.POST}) 하나의 메소드로 둘 다 처리할 때는 배열로!
     @PostMapping(value = "/login")
-    public String login(String id, String pw, Model model) throws  Exception{
+    public String login(HttpServletRequest request, String id, String pw, RedirectAttributes model) throws  Exception{
+        // RedirectAttribute 는 Model객체의 자손. 모든 기능을 가지고 있다.
+
         //1. id, pw check
         if (loginCheck(id, pw)) {
             model.addAttribute("id", id);
             model.addAttribute("pw", pw);
             return "userInfo";
         }else{
-            String msg = URLEncoder.encode("id 또는 pw가 일치하지 않읍니다.", "utf-8");
-            return "redirect:/login/login?msg="+msg; //GET방식으로 url 다시쓰기!!
+//            String msg = URLEncoder.encode("id 또는 pw가 일치하지 않읍니다.", "utf-8");
+            String msg = "id 또는 pw가 일치하지 않읍니다.";
+
+            model.addAttribute("msg", msg); //requestParam영역=> 쿼리URL에서 읽어온다. 새로고침 하면 계속 남는다.
+            model.addFlashAttribute("msg", "일회용 메시지");
+            // 세숀 영역에 저장됨. 전달되고 삭제된다. 새로고침하면 사라진다.
+            request.setAttribute("msg", "request에 저당한 msg"); // request영역에 저장한다.
+
+//            return "redirect:/login/login";
+//            return "forward:/";
+            return "redirect:/";
+//            return "redirect:/login/login?msg="+msg; //GET방식으로 url 다시쓰기!!
         }
         //2. 일치하면 userInfo.html로 이동 틀리면 login.html로 이동
 
