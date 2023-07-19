04061deabb329030489485e33511cd37c1ee8339
sungchanseo <seobanquet@gmail.com>
2023-07-19 16:36:49 +0900

    타임리프 실습

--- src/main/java/com/fastcampus/ch2/HomeController.java
+++ src/main/java/com/fastcampus/ch2/HomeController.java
@@ -6,6 +6,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 import java.util.Arrays;
 
 //1. 원격프로그램으로 먼저 등록 -> @Controller 어노테이션 추가
@@ -21,11 +24,20 @@ public class HomeController { //원격프로그램
     }
 
     @GetMapping("/test")
-    public String test(Model model){
+    public String test(Model model, HttpServletRequest request){
+        request.setAttribute("year", 2023); //request 객체에 저장
+
+        HttpSession session = request.getSession();
+        session.setAttribute("id", "asdf");
+
+        ServletContext application = session.getServletContext();
+        application.setAttribute("email", "sss@naver.com");
+
         model.addAttribute("lastName", "SEO");
         model.addAttribute("firstName", "SUNG CHAN");
         model.addAttribute("list", Arrays.asList("aaa", "bbb", "ccc", "ddd"));
         model.addAttribute("bno", "1234");
+        model.addAttribute("user", new User("aaa", 20));
         return "test"; // template/test.html
     }
 }
